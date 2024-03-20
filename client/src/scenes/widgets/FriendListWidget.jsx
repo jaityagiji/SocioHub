f6150f56ebfd4cb10e70cc61943356489134a3e6
jaityagiji <jivanshtyagi9116@gmail.com>
2024-03-20 20:37:51 +0530

    Deploying

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `http://localhost:3001/users/${userId}/friends`,
+      `https://sociohub-cjb0.onrender.com/users/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
